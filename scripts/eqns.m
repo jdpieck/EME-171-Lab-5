9af4e6ee53fabf88d507d5834ad19d9cb5c93fd2
jdpieck <143460398+jdpieck@users.noreply.github.com>
2025-12-08 23:53:08 -0800

    changes to equations

--- scripts/eqns.m
+++ scripts/eqns.m
@@ -4,6 +4,7 @@ function [ds, ext] = eqns(t, s, R_w, L_w, T_m, M, b_tau, R, G_R, C_R, g, K_p, K_
     p_9 = s(2); 
     d_ref = s(3); 
     d_actual = s(4);
+    E_in = s(5);
 
     u_in = K_p * (velocity_handle(t) - p_9/M) + K_i * (d_ref - d_actual);
 
@@ -16,7 +17,7 @@ function [ds, ext] = eqns(t, s, R_w, L_w, T_m, M, b_tau, R, G_R, C_R, g, K_p, K_
     E_dot_in = u_in * p_3 / L_w; % P_in = Voltage * Current
     E_dot_out = pdot_9 * p_9 / M; 
 
-    ds = [pdot_3; pdot_9; d_ref_dot; d_actual_dot];
+    ds = [pdot_3; pdot_9; d_ref_dot; d_actual_dot; E_dot_in];
 
     %Extra variables
     ext = [E_dot_in; E_dot_out];
