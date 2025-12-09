7b79474aff403e581c919588857f52a59c0f215f
abrzamora <alexbrianzamora@gmail.com>
2025-12-08 20:37:06 -0800

    Added fprints to assist in tuning

--- scripts/display_output.m
+++ scripts/display_output.m
@@ -35,3 +35,30 @@ grid on;
 
 % Print steady state velocity for verification
 fprintf('Final Velocity: %.4f m/s\n', v_vehicle(end));
+
+[peak,peak_time] = max(v_vehicle);
+
+fprintf('Peak Velocity: %.4f m/s\n', peak);
+fprintf('Overshoot Percentage: %.4f %% \n', (peak-v_vehicle(end))/v_vehicle(end)*100);
+fprintf('Normalized Peak Time: %.4f s \n',peak_time*t_step-t_start)
+
+
+%Initial index
+r2 = 1;
+while v_vehicle(r2) < 0.9*v_vehicle(end) %Will hit the first instance of 90% SS. Value
+    r2 = r2 + 1;
+end
+rise_time = (r2)*t_step;
+
+fprintf('Normalized Rise Time: %.4f s \n',rise_time-t_start)
+
+e = t_end/t_step;
+
+% 2% above S.S. value and 2% below.
+while v_vehicle(e) > 0.98*v_vehicle(end) && v_vehicle(e)<1.02*v_vehicle(end)
+    e = e - 1;
+end
+
+settling_time = (e-1)*t_step;
+
+fprintf('Normalized Settling Time: %.4f s \n',settling_time-t_start)
